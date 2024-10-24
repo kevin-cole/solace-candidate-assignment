cdc16a6e418023b98dd945745ef9ea035b4df7f5
Kevin Cole <kev.cole@yahoo.com>
2024-10-24 13:41:51 -0600

    Update Schema
    Don't use Text, unless value is unbounded.  Varchar will be more performant
    Use varchar for phone number
    use string array for specialties rathe than jsonb (this one could go either way, there might be a reason to use jsonb, but I  generally feel any use of json in the db should be rethought.)
    Of course specialties could be their own table and then there could be a link table between them and advocates, but I will leave the database denormalized for this task

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,22 +2,20 @@ import { sql } from "drizzle-orm";
 import {
   pgTable,
   integer,
-  text,
-  jsonb,
   serial,
   timestamp,
-  bigint,
+  varchar,
 } from "drizzle-orm/pg-core";
 
 const advocates = pgTable("advocates", {
   id: serial("id").primaryKey(),
-  firstName: text("first_name").notNull(),
-  lastName: text("last_name").notNull(),
-  city: text("city").notNull(),
-  degree: text("degree").notNull(),
-  specialties: jsonb("payload").default([]).notNull(),
+  firstName: varchar("first_name", { length: 200 }).notNull(),
+  lastName: varchar("last_name", { length: 200 }).notNull(),
+  city: varchar("city", { length: 200 }).notNull(),
+  degree: varchar("degree", { length: 5 }).notNull(),
+  specialties: varchar("specialties", { length: 200 }).array().notNull(),
   yearsOfExperience: integer("years_of_experience").notNull(),
-  phoneNumber: bigint("phone_number", { mode: "number" }).notNull(),
+  phoneNumber: varchar("phone_number", { length: 20 }).notNull(),
   createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
 });
 
